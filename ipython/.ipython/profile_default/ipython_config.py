2e4fec531389b48ce83599f825bff8a74411d178
David Silva <dfhssilva@protonmail.com>
2021-12-21 11:33:11 +0000

    Add modification to vi-mode cursor in ipython

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -1,4 +1,6 @@
 # Configuration file for ipython.
+import sys
+from prompt_toolkit.key_binding.vi_state import InputMode, ViState
 
 #------------------------------------------------------------------------------
 # InteractiveShellApp(Configurable) configuration
@@ -555,9 +557,41 @@
 #  See also: InteractiveShell.display_page
 # c.TerminalInteractiveShell.display_page = False
 
+
+def get_input_mode(self):
+    if sys.version_info[0] == 3:
+        from prompt_toolkit.application.current import get_app
+
+        app = get_app()
+        # Decrease input flush timeout from 500ms to 10ms.
+        app.ttimeoutlen = 0.01
+        # Decrease handler call timeout from 1s to 250ms.
+        app.timeoutlen = 0.25
+
+    return self._input_mode
+
+
+def set_input_mode(self, mode):
+    shape = {InputMode.NAVIGATION: 2, InputMode.REPLACE: 4}.get(mode, 6)
+    cursor = "\x1b[{} q".format(shape)
+
+    if hasattr(sys.stdout, "_cli"):
+        write = sys.stdout._cli.output.write_raw
+    else:
+        write = sys.stdout.write
+
+    write(cursor)
+    sys.stdout.flush()
+
+    self._input_mode = mode
+
+
+ViState._input_mode = InputMode.INSERT
+ViState.input_mode = property(get_input_mode, set_input_mode)
+
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
 #  Default: 'emacs'
-c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.editing_mode = "vi"
 
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
 #  Default: 'vi'
