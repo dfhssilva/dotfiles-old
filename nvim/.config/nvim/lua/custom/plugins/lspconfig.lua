15716d01c2b8a2ddd9f9e9a70fa419c2554cdf14
David Silva <dfhssilva@protonmail.com>
2021-12-29 21:18:34 +0000

    Add nvim pyright lsp settings

--- nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -2,28 +2,44 @@ local M = {}
 M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
 
-   -- lspservers with default config
-   local servers = { "pyright", "dockerls", "ltex" }
-   
-   -- lspservers executables - NEEDS TO SPECIFY THE EXECUTABLE FOR EVERY SERVER
-   local executables = { 
-     { "/home/dsilva/.local/share/nvim/lsp_servers/python/node_modules/pyright/langserver.index.js", "--stdio" }, 
-     { "/home/dsilva/.local/share/nvim/lsp_servers/dockerfile/node_modules/dockerfile-language-server-nodejs/bin/docker-langserver", "--stdio" },
-     { "/home/dsilva/.local/share/nvim/lsp_servers/ltex/ltex-ls-15.2.0/bin/ltex-ls" }
-   }
-   
-   -- lspservers default configurations
-   for i, lsp in ipairs(servers) do
-     lspconfig[lsp].setup {
-       cmd = executables[i],
-       on_attach = attach,
-       capabilities = capabilities,
-       flags = {
-         debounce_text_changes = 150,
-       },
+   -- pyright server configuration
+   lspconfig.pyright.setup {
+     cmd = { "/home/dsilva/.local/share/nvim/lsp_servers/python/node_modules/pyright/langserver.index.js", "--stdio" }, 
+     on_attach = attach,
+     capabilities = capabilities,
+     flags = {
+       debounce_text_changes = 150,
+     },
+     settings = {
+       python = {
+         analysis = {
+           useLibraryCodeForTypes = true
+         },
+         venvPath = "/home/dsilva/miniconda3/envs"
+       }
      }
-   end
+   }
+
+   -- dockerls server configuration
+   lspconfig.dockerls.setup {
+     cmd = { "/home/dsilva/.local/share/nvim/lsp_servers/dockerfile/node_modules/dockerfile-language-server-nodejs/bin/docker-langserver", "--stdio" },
+     on_attach = attach,
+     capabilities = capabilities,
+     flags = {
+       debounce_text_changes = 150,
+     },
+   }
   
+   -- ltex server configuration
+   lspconfig.ltex.setup {
+     cmd = { "/home/dsilva/.local/share/nvim/lsp_servers/ltex/ltex-ls-15.2.0/bin/ltex-ls" },
+     on_attach = attach,
+     capabilities = capabilities,
+     flags = {
+       debounce_text_changes = 150,
+     },
+   }
+
    -- texlab server configuration
    lspconfig.texlab.setup {
     cmd = { "/home/dsilva/.local/share/nvim/lsp_servers/latex/texlab" },
